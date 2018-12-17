b195268fa825f0577d240b5afb4fd3376f0ebc88
Muhammed Kılıç <muhammeddkilicc@gmail.com>
2018-12-17 15:17:26 +0300

    added sample proxy

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
-
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class Main {
 
     public static void main(String args[]) throws Exception {
 
+
+        MyProxy myProxy = new MyProxy(8888);
+        myProxy.listen();
+/*
         ServerSocket Server = new ServerSocket(5000, 10, InetAddress.getByName("127.0.0.1"));
-        System.out.println("TCPServer Waiting for client on port 5000");
+        System.out.printf("Waiting for client on port %d on address %s ...%n", Server.getLocalPort(), Server.getInetAddress());
 
         while (true) {
             try {
@@ -17,7 +16,6 @@ public class Main {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
-        }
+        }*/
     }
 }
