fb5f7f40d2a9fe63b90e3368ce4b3d4024bd3c38
kiliczsh <muhammeddkilicc@gmail.com>
2018-12-23 19:52:23 +0300

    scripts added

---
+++ run-http.sh
@@ -0,0 +1,4 @@
+echo "starting http server"
+cd http-server/
+. web/bin/activate
+python3 http_server.py 5000
---
+++ run-proxy.sh
@@ -0,0 +1,4 @@
+echo "starting proxy server"
+cd proxy-server/src/
+javac *.java
+java ProxyMain
