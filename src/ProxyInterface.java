6709f3aedca433c31cfcac99dcae507569f4d358
Berkay <berkayyaman95@gmail.com>
2018-12-23 18:36:44 +0300

    Added: New error messages

--- src/ProxyInterface.java
+++ src/ProxyInterface.java
@@ -2,10 +2,12 @@ public interface ProxyInterface {
     String typeGET = "GET";
     String notFound = "Not Found";
     String tooLong = "Request-URI Too Long";
+    String badRequest = "Bad Request";
 
     String notFoundStatus = "404 Not Found";
     String tooLongStatus = "414 Request-URI Too Long";
     String okStatus = "200 OK";
+    String badRequestStatus = "400 Bad Request";
 
     String version = "HTTP/1.0";
 
@@ -16,17 +18,25 @@ public interface ProxyInterface {
         private String requestType;
         String address;
         int port;
+        String errorCode;
         ReturnFormat(String requestType,String address,int port,String uri){
             this.requestType = requestType;
             this.address = address;
             this.port = port;
             this.uri = uri;
+            this.errorCode = null;
         }
+
         ReturnFormat(String requestType,String uri){
             this.requestType = requestType;
+            this.errorCode = null;
             this.address = "localhost";
             this.port = 8080;
             this.uri = uri;
+        }
+        ReturnFormat(String errorCode){
+
+            this.errorCode = errorCode;
         }
         String produceString(){
             return requestType+" /"+uri+" "+version+"\r\n";
